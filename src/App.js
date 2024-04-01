8fb495043bed28074947c06ac0adebf1c7515312
abbiedesignco <abbier.design@gmail.com>
2024-04-01 10:32:43 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.abbiericher.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Abbie Richer
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/abbiedesignco/react-weather-app-w5-abbie"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://sweet-malasada-7a8370.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="App-container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.abbiericher.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Abbie Richer
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/abbiedesignco/react-weather-app-w5-abbie"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://sweet-malasada-7a8370.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
